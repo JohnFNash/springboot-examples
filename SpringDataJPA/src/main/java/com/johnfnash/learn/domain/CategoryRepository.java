8ee3cd7000563a6e38835de4396ccaddedc16e65
JohnFNash <JohnFNash@users.noreply.github.com>
2018-06-10 18:00:56 +0800

    优化结构

--- SpringDataJPA/src/main/java/com/johnfnash/learn/domain/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.johnfnash.learn.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
