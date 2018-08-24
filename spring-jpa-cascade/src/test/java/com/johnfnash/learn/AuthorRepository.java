99de9a6e2dfb567f3f16c3d7bd32f1a0e16c1abc
JohnFNash <JohnFNash@users.noreply.github.com>
2018-08-24 09:47:43 +0800

    test of OneToMany

--- spring-jpa-cascade/src/test/java/com/johnfnash/learn/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.johnfnash.learn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.johnfnash.learn.domain.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-}
